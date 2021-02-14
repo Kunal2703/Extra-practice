78569e5a6f79ad8355e38ebb6240c5ed375226a4
kunalsingh27 <78562069+kunalsingh27@users.noreply.github.com>
2021-02-13 22:46:56 -0800

    Add files via upload

---
+++ form 3x3 matrix.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void main()
+{
+  int arr[3][3],i,j;
+
+       printf("Input elements in the matrix :\n");
+  for(i=0;i<3;i++)
+  {
+      for(j=0;j<3;j++)
+      {
+	      printf("element - [%d],[%d] : ",i,j);
+	      scanf("%d",&arr[i][j]);
+      }
+  }
+
+ printf("\nThe matrix is : \n");
+  for(i=0;i<3;i++)
+  {
+      printf("\n");
+      for(j=0;j<3;j++)
+           printf("%d\t",arr[i][j]);
+  }
+ printf("\n\n");
+}
