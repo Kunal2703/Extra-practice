61665230d940a07da39acf4c8d1054c43fffa9b2
kunalsingh27 <78562069+kunalsingh27@users.noreply.github.com>
2021-02-13 23:04:36 -0800

    Add files via upload

---
+++ Store ‘n’ integers in an array in decending  order.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+
+{
+    int number[100];
+    int i,j,m,n;
+    printf("Enter the value of N\n");
+    scanf("%d",&n);
+
+    printf("Enter the numbers \n");
+    for(i=0;i<n;i++)
+        scanf("%d",&number[i]);
+
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(number[i]>number[j])
+            {
+                m=number[i];
+                number[i]=number[j];
+                number[j]=m;
+            }
+        }
+    }
+    printf("The  number arrange in descending order are given below\n");
+
+    for(i=0;i<n;i++)
+    {
+        printf("%d\n",number[i]);
+    }
+    return 0;
+
+}
